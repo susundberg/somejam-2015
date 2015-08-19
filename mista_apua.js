699364d446c866c357690e3cb6420feac688ad21
P Sundberg <susundberg@gmail.com>
2015-08-19 20:44:47 +0300

    Add CSS loading function

--- mista_apua.js
+++ mista_apua.js
@@ -3,19 +3,26 @@
 // Localize jQuery variable
 var jQuery;
 
-var link_tag = document.createElement('link');
+
+function create_link_tag( url )
+{
+  var link_tag = document.createElement('link');
   link_tag.setAttribute("type", "text/css");
   link_tag.setAttribute("rel", "stylesheet");
-  link_tag.setAttribute("href", "https://cdn.rawgit.com/yahoo/pure-release/v0.6.0/pure-min.css");
-  link_tag.onload = styleLoadHandler;
-  (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(link_tag);
+  link_tag.setAttribute("href", url );
+  return link_tag;
+}
 
 // From http://alexmarandon.com/articles/web_widget_jquery/
 var script_tag = document.createElement('script');
   script_tag.setAttribute("type", "text/javascript");
   script_tag.setAttribute("src", "https://code.jquery.com/jquery-1.11.1.min.js");
   script_tag.onload = scriptLoadHandler;
-  (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+
+var doc_head = (document.getElementsByTagName("head")[0] || document.documentElement);
+doc_head.appendChild( create_link_tag( "https://cdn.rawgit.com/yahoo/pure-release/v0.6.0/pure-min.css" ) );
+doc_head.appendChild( create_link_tag( "https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" ) );
+doc_head.appendChild(script_tag);
 
 // Called once jQuery has loaded 
 function scriptLoadHandler() {
@@ -24,10 +31,6 @@ function scriptLoadHandler() {
     jQuery = window.jQuery.noConflict(true);
     // Call our main function
     main(); 
-}
-
-function styleLoadHandler() {
-  
 }
 
 function main() { 
@@ -47,42 +50,40 @@ jQuery(document).ready(function($) {
   // by its id. This creates lots of queries .. 
   function event_get_location( event_id )
   {
-     var params = [ api_key, "fields=description,location" ];
+     var params = [ api_key, "fields=description,location,colorId,kind,transparency,visibility" ];
      var get_url = base_url + "/" + event_id + "/" +  "?" + params.join("&");
      $.getJSON( get_url, function( data ) { 
-       var target_url = data["location"];
-       // Make sure we dont get relative links .. 
-       if ( target_url.slice(0,4) != "http" )
-       {
-         target_url = "http://" + target_url;
-       }
        var elem = $("#" + event_id );   
-       elem.attr("href",  target_url );
+       console.log(JSON.stringify(data));
+       var target_url = data["location"];
+       if ( target_url ) // Avoid errors if somebody forgets to update the location
+       {
+          // Make sure we dont get relative links .. 
+          if ( target_url.slice(0,4) != "http" )
+          {
+            target_url = "http://" + target_url;
+          }
+          elem.attr("href",  target_url );
+       }
        elem.attr("title", data["description"]);
      } );
   }
   
-  var current_day = Math.floor( now / 1000 / 60 / 60 / 24); 
   $.getJSON( get_events_url , function( data ) {
     var table_items = [];
     $.each( data["items"], function( key, val ) {
 
        var date_start = new Date( val.start.dateTime );
        var date_end   = new Date( val.end.dateTime );
-       var event_start_date = Math.floor( date_start / 1000 / 60 / 60 / 24); 
-       var str_desc = " "
-
-       function pad2(number) { return (number < 10 ? '0' : '') + number };
-       function dformat( d ) { return pad2(d.getHours()) + ":" + pad2(d.getMinutes()) };
        
-       // Produce the date of the event if not today
-       if ( event_start_date != current_day )
+       if ( date_start > now || now > date_end )
        {
-         str_desc = pad2( date_start.getDate() ) + "." ;
+         // skip all events that are not open right now
+         return;
        }
-       
-       var elems = [ "<a class='pure-button' id='"+val.id+"'> " + val.summary + "</a>" , str_desc, dformat(date_start),dformat(date_end) ];
-       table_items.push( "<td>" + elems.join("</td><td>") + "</td>" );
+     
+       var elems = [ "<a class='pure-button' id='"+val.id+"' style='width:100%;'> " + val.summary + "</a>" , "<i class='fa fa-users'></i>" ];
+       table_items.push( "<td style='border-left:none;'> " + elems.join("</td><td style='border-left:none;'>") + "</td>" );
        event_get_location( val.id );
     });
     $("#mistaapua").append("<table class='pure-table'> <tbody><tr>" + table_items.join("</tr><tr>") + "</tr></table>" );
